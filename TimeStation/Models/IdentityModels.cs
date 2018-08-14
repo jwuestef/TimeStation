fc93779796797816247fb5113f2248a77dea07d3
jwuestef <jwuestef@gmail.com>
2018-08-14 16:42:04 -0400

    connected to PostgreSQL

--- TimeStation/Models/IdentityModels.cs
+++ TimeStation/Models/IdentityModels.cs
@@ -9,6 +9,16 @@ namespace TimeStation.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        //public Barcode Barcode { get; set; }
+        public string UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        //public Department Department { get; set; }
+        //public Role Role { get; set; }
+        //public Cohort? Cohort { get; set; }
+        //public Campus Campus { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -21,7 +31,7 @@ namespace TimeStation.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("DefaultPostgreSQLConnection", throwIfV1Schema: false)
         {
         }
 
---
+++ TimeStation/Models/Role.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TimeStation.Models
+{
+    public class Role
+    {
+        // These are here to stop us using 0 and 1 in the code. Could use an enum too, but it would be a little more complicated... easier to do this
+        public static readonly byte Guest = 0;
+        public static readonly byte Student = 1;
+        public static readonly byte Staff = 2;
+        public static readonly byte Admin = 3;
+    }
+}
