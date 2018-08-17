c108e091cf895516b1097f23aa5e34c729879172
jwuestef <jwuestef@gmail.com>
2018-08-17 15:43:26 -0400

    CRUD with cohorts finished adding

--- TimeStation/Models/Cohort.cs
+++ TimeStation/Models/Cohort.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,15 @@ namespace TimeStation.Models
 {
     public class Cohort
     {
+
+        [Required(ErrorMessage = "Cohort ID is required.")]
+        [StringLength(255, ErrorMessage = "Cohort ID must be less than 255 characters.")]
+        [Display(Name = "Cohort ID")]
         public string CohortId { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(255, ErrorMessage = "Name must be less than 255 characters.")]
+        [Display(Name = "Name")]
         public string CohortName { get; set; }
 
 
