ca8f443a7fd2ba73420ed5a6dcf7189421ff7110
jwuestef <jwuestef@gmail.com>
2018-08-21 13:55:44 -0400

    Finally got Attendance views generated successfully... currently working on Create view

---
+++ TimeStation/Controllers/AttendancesController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TimeStation.Models;
+
+namespace TimeStation.Controllers
+{
+    public class AttendancesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Attendances
+        public ActionResult Index()
+        {
+            var users = db.Users
+                .Include(user => user.Department)
+                .Include(user => user.Cohort)
+                .Include(user => user.Campus)
+                .ToList();
+
+            return View(users);
+        }
+
+        // GET: Attendances/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Attendance attendance = db.Attendances.Find(id);
+            if (attendance == null)
+            {
+                return HttpNotFound();
+            }
+            return View(attendance);
+        }
+
+        // GET: Attendances/Create
+        public ActionResult Create()
+        {
+            var viewModel = new CreateAttendanceViewModel
+            {
+                ApplicationUsers = db.Users.ToList(),
+                Campuses = db.Campuses.ToList(),
+                Departments = db.Departments.ToList(),
+                Cohorts = db.Cohorts.ToList()
+            };
+
+            return View(viewModel);
+        }
+
+        // POST: Attendances/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AttendanceId,ApplicationUserId,TimeIn,TimeOut,Duration,CampusId,DepartmentId,CohortId")] Attendance attendance)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Attendances.Add(attendance);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(attendance);
+        }
+
+        // GET: Attendances/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Attendance attendance = db.Attendances.Find(id);
+            if (attendance == null)
+            {
+                return HttpNotFound();
+            }
+            return View(attendance);
+        }
+
+        // POST: Attendances/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AttendanceId,ApplicationUserId,TimeIn,TimeOut,Duration,CampusId,DepartmentId,CohortId")] Attendance attendance)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(attendance).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(attendance);
+        }
+
+        // GET: Attendances/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Attendance attendance = db.Attendances.Find(id);
+            if (attendance == null)
+            {
+                return HttpNotFound();
+            }
+            return View(attendance);
+        }
+
+        // POST: Attendances/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Attendance attendance = db.Attendances.Find(id);
+            db.Attendances.Remove(attendance);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TimeStation/Models/CreateAttendanceViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TimeStation.Models
+{
+    public class CreateAttendanceViewModel
+    {
+
+        public int AttendanceId { get; set; }
+
+        [Required(ErrorMessage = "User is required.")]
+        [Display(Name = "User")]
+        public string ApplicationUserId { get; set; }
+        public IEnumerable<ApplicationUser> ApplicationUsers { get; set; }
+
+        [Display(Name = "Time In")]
+        public DateTime TimeIn { get; set; }
+
+        [Display(Name = "Time Out")]
+        public DateTime? TimeOut { get; set; }
+
+        public TimeSpan? Duration { get; set; }
+
+        [Display(Name = "Campus")]
+        public int CampusId { get; set; }
+        public IEnumerable<Campus> Campuses { get; set; }
+
+        [Display(Name = "Department")]
+        public int DepartmentId { get; set; }
+        public IEnumerable<Department> Departments { get; set; }
+
+        [Display(Name = "Cohort")]
+        public string CohortId { get; set; }
+        public IEnumerable<Cohort> Cohorts { get; set; }
+
+
+    }
+}
